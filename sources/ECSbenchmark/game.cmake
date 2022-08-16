4fa500df03e2a3312424c60f5730d2ba27171ec8
Roman <rodionovv22@gmail.com>
2022-08-16 21:19:21 +0300

    Base project

--- sources/ECSbenchmark/game.cmake
+++ sources/ECSbenchmark/game.cmake
@@ -8,11 +8,9 @@ include(libs.cmake)
 
 include_library(eastl 3rd_party/eastl)
 include_library(ecs ecs)
-include_library(das das)
 set(RENDER_ON OFF)
 include_library(engine engine)
 
 include_library(imgui 3rd_party/imgui)
-include_library(libDaScript 3rd_party/daScript)
 include_library(ECSbenchmarkLib ECSbenchmark)
 
